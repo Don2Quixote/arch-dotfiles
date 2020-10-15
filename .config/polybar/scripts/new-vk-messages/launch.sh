032b2fd0a3e88dde326ea3d8d74febfa4a169a87
Don2Quixote <idonquixote@yandex.ru>
2020-10-15 23:38:15 +0300

    Added polybar config files

--- .config/polybar/scripts/new-vk-messages/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-echo $(node ~/.config/polybar/scripts/new-vk-messages/main.js)
